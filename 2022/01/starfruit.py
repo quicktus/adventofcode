bbeb3369fe76e7a906d7cc7c58baf62aa669f2a7
Kilian Wenker <24503963+quicktus@users.noreply.github.com>
2022-12-01 11:13:27 +0100

    day 1

---
+++ 2022/01/starfruit.py
@@ -0,0 +1,20 @@
+import os
+
+# read input from file
+input = open(os.path.join(os.getcwd(),"./input.txt"), "r")
+lines = input.readlines()
+
+elf_list = []
+# calculate sums
+cal_sum = 0
+for line in lines:
+    line = line.strip()
+    if line == "":
+        elf_list.append(cal_sum)
+        cal_sum = 0
+    else:
+        cal_sum += int(line)
+
+elf_list.sort(reverse=True)
+n = 3
+print("top {} elfs: {}  sum: {}".format(n,elf_list[:n], sum(elf_list[:n])))
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Advent of Code
 ## 2021
 For my first year, I chose C. Due to time constraints, I only aimed to get the first star of each puzzle. I made it to day 19, which I'm quite happy with overall.
+
+## 2021
+This year, I went with python. Due to even more time constraints, I restricted myself to 30 minutes per puzzle.
