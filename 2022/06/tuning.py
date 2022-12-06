d9110a25157fc19001f7e0c6db2f99c4e2d4c02d
Kilian Wenker <24503963+quicktus@users.noreply.github.com>
2022-12-06 11:58:24 +0100

    I committed war crimes in python

--- 2022/06/tuning.py
+++ 2022/06/tuning.py
@@ -1,16 +1,1 @@
-import os
-from collections import Counter
-
-def find_seq(signal, seq_len):
-    for i in range(seq_len, len(signal)):
-        if(len(Counter(signal[(i-seq_len):i])) == seq_len):
-            return i
-
-input = open(os.path.join(os.getcwd(),"input.txt"), "r")
-line = input.readline()
-pgk_seq_len, msg_seq_len = 4, 14
-
-print(f"first package sequence found after {find_seq(line, pgk_seq_len)} chars")
-print(f"first message sequence found after {find_seq(line, msg_seq_len)} chars")
-
-# [len(Counter(signal[(i-seq_len):i])) for i in range(seq_len, len(signal))].index(seq_len)
+import os;from collections import Counter as c;l=open(os.path.join(os.getcwd(),"input.txt"),"r").read();x=len(l);s="\nfirst {}age sequence found after {} chars";print(s.format("pack",[len(c(l[i-4:i]))for i in range(4,x)].index(4)),s.format("mess",[len(c(l[i-14:i]))for i in range(14,x)].index(14)))
