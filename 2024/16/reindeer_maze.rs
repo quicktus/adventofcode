9ea55fcacff7a080a7b5cc624070d3e249149840
quicktus <kilian.wenker@fau.de>
2025-01-04 22:22:15 +0100

    day 16 (part 2)

--- 2024/16/reindeer_maze.rs
+++ 2024/16/reindeer_maze.rs
@@ -1,10 +1,52 @@
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::Read;
-use std::io::{stdin, stdout, Write};
 use std::path::Path;
-use pathfinding::prelude::dijkstra;
+use std::collections::{BinaryHeap, HashMap};
 
-static mut MAZE: Vec<Vec<char>> = Vec::new();
+#[derive(Eq, Hash, PartialEq, Clone)]
+struct Position {
+    coords: (usize, usize),
+    direction: (i32, i32),
+}
+
+impl Position {
+    fn successors(&self, maze: &Vec<Vec<char>>, initial_cost: u32) -> Vec<(Position, u32)> {
+        let mut s: Vec<(Position, u32)> = vec![];
+
+        // forward
+        let next = apply_offset(self.coords, self.direction);
+        if maze[next.1][next.0] != '#' {
+            s.push((
+                Position {
+                    coords: next,
+                    direction: self.direction,
+                },
+                initial_cost + 1,
+            ));
+        }
+
+        // left
+        s.push((Position {coords: self.coords, direction: rot_left(self.direction),}, initial_cost + 1000,));
+
+        // right
+        s.push((Position {coords: self.coords, direction: rot_right(self.direction),}, initial_cost + 1000,));
+
+        s
+    }
+}
+impl Ord for Position {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.coords.cmp(&self.coords)
+    }
+}
+
+impl PartialOrd for Position {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 
 fn main() {
     let path = Path::new("input.txt");
@@ -20,72 +62,59 @@ fn main() {
         Ok(_) => (),
     }
 
-    #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-    struct Pos{coords: (usize, usize), direction: (i32, i32)}
+    let maze: Vec<Vec<char>> = input.lines().map(|x| x.chars().collect()).collect();
+    let (x_len, y_len) = (maze[0].len(), maze.len());
 
-    impl Pos {
-        unsafe fn successors(&self) -> Vec<(Pos, u32)> {
-            let mut s= vec![
-                // left
-                (Pos {coords: self.coords, direction: rot_left(self.direction)}, 1000),
-                // right
-                (Pos {coords: self.coords, direction: rot_right(self.direction)}, 1000),
-            ];
+    let mut start_idx: (usize, usize) = (0, 0);
+    let mut finish_idx: (usize, usize) = (0, 0);
 
-            // forward
-            let next = apply_offset(self.coords, self.direction);
-            if MAZE[next.1][next.0] != '#' {
-                s.push((Pos {coords: next, direction: self.direction}, 1));
+    for x in 0..x_len {
+        for y in 0..y_len {
+            if maze[y][x] == 'S' {
+                start_idx = (x, y);
             }
-
-            s
+            if maze[y][x] == 'E' {
+                finish_idx = (x, y);
+            }
         }
     }
 
-    unsafe {
-        MAZE = input.lines().map(|x| x.chars().collect()).collect();
-        let (x_len, y_len) = (MAZE[0].len(), MAZE.len());
+    let start_pos: Position = Position {coords: start_idx, direction: (1, 0),};
+    let mut visited: HashMap<Position, (u32, Vec<Position>)> = HashMap::new();
+    let mut queue = BinaryHeap::new();
+    queue.push((std::cmp::Reverse(0), start_pos.clone(), vec![start_pos]));
+    let mut res_1 = u32::MAX;
 
-        let mut start_idx: (usize, usize) = (0, 0);
-        let mut finish_idx: (usize, usize) = (0, 0);
+    let mut coords_in_shortest_paths: Vec<(usize, usize)> = Vec::new();
 
-        for x in 0..x_len {
-            for y in 0..y_len {
-                if MAZE[y][x] == 'S' {
-                    start_idx = (x, y);
-                }
-                if MAZE[y][x] == 'E' {
-                    finish_idx = (x, y);
-                }
-            }
+    while let Some((std::cmp::Reverse(cost), current_pos, path)) = queue.pop() {
+        if cost > res_1 {
+            break;
         }
 
-        let (path, result) = dijkstra(&Pos{coords: start_idx, direction: (0, 1)}, |p| p.successors(), |p| p.coords == finish_idx).unwrap();
+        visited.insert(current_pos.clone(), (cost, path.clone()));
+        if current_pos.coords == finish_idx {
+            res_1 = cost;
 
-        for pos in path {
-            for x in 0..x_len {
-                for y in 0..y_len {
-                    if (x, y) == pos.coords {
-                        let dirchar = match pos.direction {
-                            (0, 1)  => '>',
-                            (0, -1) => '<',
-                            (1, 0)  => 'v',
-                            (-1, 0) => '^',
-                            _ => panic!()
-                        };
-                        print!("\x1b[93m{dirchar}\x1b[0m");
-                    }
-                    else {
-                        print!("{}", MAZE[y][x]);
-                    }
-                }
-                println!();
+            for pos in path {
+                coords_in_shortest_paths.push(pos.coords);
             }
-            pause();
+            coords_in_shortest_paths.sort();
+            coords_in_shortest_paths.dedup();
+            continue;
         }
 
-        println!("part 1: {}", result);
+        for (next_pos, next_cost) in current_pos.successors(&maze, cost) {
+            if !visited.contains_key(&next_pos) || next_cost < visited[&next_pos].0 {
+                let mut new_path = path.clone();
+                new_path.push(next_pos.clone());
+                queue.push((std::cmp::Reverse(next_cost), next_pos, new_path));
+            }
+        }
     }
+    println!("part 1: {}", res_1);
+    println!("part 2: {}", coords_in_shortest_paths.len());
+
 }
 
 fn apply_offset(point: (usize, usize), offset: (i32, i32)) -> (usize, usize) {
@@ -107,11 +136,4 @@ fn rot_left(direction: (i32, i32)) -> (i32, i32) {
         return (direction.1, -direction.0);
     }
     (direction.1, direction.0)
-}
-
-fn pause() {
-    let mut stdout = stdout();
-    stdout.write(b"Press Enter to continue...").unwrap();
-    stdout.flush().unwrap();
-    stdin().read(&mut [0]).unwrap();
 }
